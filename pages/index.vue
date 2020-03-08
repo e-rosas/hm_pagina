750268dff20b0e4697ad110eac4d7ffaa68ace15
Edgar Rosas <gary.blurk@gmail.com>
2020-03-07 22:18:39 -0800

    index

--- pages/index.vue
+++ pages/index.vue
@@ -19,6 +19,7 @@
         >
           GitHub
         </a>
+        <nuxt-link to="promos">Register</nuxt-link>
       </div>
     </div>
   </div>
---
+++ pages/promos.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <b-form v-if="show" @submit="onSubmit" @reset="onReset">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Birth date:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="form.food"
+          type="date"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" label="Insured">
+        <b-form-radio value="1">Yes</b-form-radio>
+        <b-form-radio value="0">No</b-form-radio>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        insured: 0
+      },
+      foods: [
+        { text: 'Select One', value: null },
+        'Carbrots',
+        'Beans',
+        'Tomatoes',
+        'Corn'
+      ],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset(evt) {
+      evt.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+  }
+}
+</script>
